fae067042aa40cd3007a7277e3aa6e7bf22e8dc4
Terry Tay <terry@resynctech.com>
2021-06-14 17:54:39 +0800

    scaffold nextjs with tailwind, typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="h-screen flex items-center bg-gray-100">
+      <div className="flex flex-col w-3/4 text-gray-500 text-center items-center justify-center bg-white rounded-lg shadow-md mx-auto h-32">
+        <button className="button">This is a custom button</button>
+        <span className="link">This is a custom link</span>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
